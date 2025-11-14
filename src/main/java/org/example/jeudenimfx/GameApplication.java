08f145b3625236316a5650ea1d1f0346029d141a
maxence468 <maxencebarth12@gmail.com>
2025-11-14 18:23:05 +0100

    joli

--- src/main/java/org/example/jeudenimfx/GameApplication.java
+++ src/main/java/org/example/jeudenimfx/GameApplication.java
@@ -4,8 +4,16 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
 
 import static javafx.geometry.Pos.*;
 
@@ -28,35 +36,54 @@ public class GameApplication extends Application {
     int MAX_RETRAIT = 3;
     //savoir si la partie est finie
     boolean partieFinie = false;
+    //recuperer une police personalisée
+    Font font = Font.loadFont(getClass().getResourceAsStream("/Monocraft.ttf"), 17);
+
 
     @Override
     public void start(Stage stage) throws Exception {
         //Affichage du tour
         Label inviteDeSaisie = new Label("Joueur"+ joueur[nbTour] + " Combien d'allumette veux-tu enlever ?");
+        inviteDeSaisie.setFont(font);
+
         //Champ input
         TextField reponse = new TextField();
+
         //boutton validation de la saisie
         Button btnValider = new Button("retirer");
+
         //Affichage des allumettes restantes
         Label lblStatutPile  = new Label("| | | | | | | | | |");
+        lblStatutPile.setFont(Font.font("Stencil",  FontWeight.BOLD, 40 ));
+        lblStatutPile.setTextFill(Color.BROWN);
+
         //Affiche le resultat de chaque tour
         Label lblFeedback  = new Label();
+        lblFeedback.setFont(font);
+        lblFeedback.setTextFill(Color.GREY);
+
         //Bouton pour rejouer
         Button btnRejouer = new Button("Rejouer");
+
         //image de l'icon
         Image icon = new Image("/nim.png");
+
+        Image image = new Image("/image.png");
+        ImageView imageView = new ImageView(image);
+        imageView.setFitHeight(200);
+        imageView.setFitWidth(200);
 
         //boite Colonne verticale avec 8 d'espace
         VBox layout = new VBox(8);
         //Ajout des composants dans la boite verticale
-        layout.getChildren().addAll(inviteDeSaisie,reponse,btnValider,lblStatutPile,lblFeedback,btnRejouer);
+        layout.getChildren().addAll(imageView, inviteDeSaisie,lblFeedback,reponse,btnValider,lblStatutPile,btnRejouer);
         btnRejouer.setVisible(false);
 
         //alignement de tous les elements au centre
         layout.setAlignment(CENTER);
 
         // 2. Création de la Scene (attachement du nœud racine + dimensions)
-        Scene scene = new Scene(layout, 400, 400);
+        Scene scene = new Scene(layout, 600, 600);
 
 
         // 3. Attachement de la Scene au Stage
@@ -78,15 +105,21 @@ public class GameApplication extends Application {
                 //on recupere le choix du joueur et le converti en int
                 try {
                     choix = Integer.parseInt(reponse.getText());
+                    reponse.clear();
                 }
-                catch(Exception ex) {
-                    System.out.println("pas un nombre");
-                    lblFeedback.setText("Il faut choisir un NOMBRE entre 1 et 3");
+                catch(Exception _) {
+
                 }
             }
 
             //Verification du choix
             if(choix <= 0 || choix > MAX_RETRAIT){
+                lblFeedback.setText("Il faut choisir un nombre entre 1 et 3");
+                try {
+                    wait(500);
+                } catch (InterruptedException ex) {
+                    throw new RuntimeException(ex);
+                }
                 lblFeedback.setText("Il faut choisir un nombre entre 1 et 3");
             }
             else if(choix > taillePileActuelle){
